5b3d5fb5f2d8e0a1b0dc10e468438f946cfd2a0f
Harsh Raj <harshraj6982@gmail.com>
2023-10-05 22:45:11 +0530

    Added Sounds

--- attendance.py
+++ attendance.py
@@ -2,43 +2,72 @@ import cv2
 import face_recognition
 import os
 import numpy as np
+from gtts import gTTS
 from datetime import datetime
 
+# Define the path to the directory containing user face images
 path = 'user_faces'
 
+# Lists to store user images and their corresponding class names (names of users)
 images = []
 classNames = []
 mylist = os.listdir(path)
 
+# Load user images and class names from the 'user_faces' directory
 for cl in mylist:
     if cl.endswith(".jpg") or cl.endswith(".png"):
         curImg = cv2.imread(f'{path}/{cl}')
         images.append(curImg)
         classNames.append(os.path.splitext(cl)[0])
 
+# Function to play an audio sound using 'afplay' (macOS specific)
+def play_sound(path):
+    os.system(f'afplay {path}')  # Play the audio file
+
+# Function to encode faces in user images
 def findEncodings(images):
+    play_sound("sounds/encoding.mp3")  # Play an encoding sound
     encodeList = []
     for img in images:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         encoded_face = face_recognition.face_encodings(img)[0]
         encodeList.append(encoded_face)
+    play_sound("sounds/encoded.mp3")  # Play an encoded sound
     return encodeList
 
+# Encode the faces in user images and store the encodings
 encoded_face_train = findEncodings(images)
 
+# Function to mark attendance and play a sound message
 def markAttendance(name):
     if name != "Unknown":
-        with open('Attendance.csv', 'r+') as f:
-            myDataList = f.readlines()
-            nameList = [entry.split(',')[0].strip() for entry in myDataList]
-            if name not in nameList:
-                now = datetime.now()
-                time = now.strftime('%I:%M:%S:%p')
-                date = now.strftime('%d-%B-%Y')
-                f.writelines(f'{name}, {time}, {date}\n')
+        # Create a set to store existing names for faster lookup
+        existing_names = set()
 
+        with open('Attendance.csv', 'r') as f:
+            for line in f:
+                entry = line.split(',')
+                existing_names.add(entry[0].strip())
+
+        if name not in existing_names:
+            now = datetime.now()
+            time = now.strftime('%I:%M:%S:%p')
+            date = now.strftime('%d-%B-%Y')
+            with open('Attendance.csv', 'a') as f:
+                f.writelines(f'{name}, {time}, {date}\n')
+            
+            # Use gTTS to speak the attendance message
+            message = f'{name} attendance has been marked'
+            tts = gTTS(message)
+            tts.save('attendance_message.mp3')
+            os.system('afplay attendance_message.mp3')  # Play the audio file
+            os.remove('attendance_message.mp3')  # Delete the temporary audio file
+
+# Initialize the webcam capture
 cap = cv2.VideoCapture(0)
 known_face_names = []
+
+# Main loop for capturing frames and processing faces
 while True:
     success, img = cap.read()
     imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
@@ -62,7 +91,7 @@ while True:
             known_face_names.append(name)
 
             if known_face_names.count(name) >= 5:
-                markAttendance(name)
+                markAttendance(name)  # Mark attendance only if recognized consistently
                 known_face_names.clear()
 
     for (top, right, bottom, left), name in zip(faces_in_frame, face_names):
@@ -76,5 +105,6 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+# Release the webcam and close all OpenCV windows
 cap.release()
 cv2.destroyAllWindows()
